5a305236fc76bd9dd33da66f8368e56361826d8c
aaronh1986 <aaronharbourne@gmail.com>
2021-04-09 15:07:37 +0000

    Project finished

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 // Listen for form submit
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-// Save Bookmark
+//Save bookmark
 function saveBookmark(e) {
-
-    //Get form values
+    // Get form values
     var siteName = document.getElementById('siteName').value;
     var siteUrl = document.getElementById('siteUrl').value;
 
-    // Storing the info in an object
+    if (!siteName || !siteUrl) {
+        alert('Please, will you fill in the ******* form.');
+        return false;
+    }
+
     var bookmark = {
         name: siteName,
         url: siteUrl,
     }
 
     /*
-    //Local storage test
-    localStorage.setItem('Michael', "Michael Greaves was kinda better than DANZIG in my humble opinion.");
-    console.log(localStorage.getItem('Michael'));
-    localStorage.removeItem('Michael');
-    console.log(localStorage.getItem('Michael'));
+    //Local storage text(Local storage only stores strings)
+    // Below is standard way of getting info from local storage
+    localStorage.setItem('test', 'Hello world');
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+    console.log(localStorage.getItem('test'));
     */
 
-    // Test if bookmarks = null
+    //We want to save the bookmark in local storage
+    //But first we have to check if there's a bookmark value already there
     if (localStorage.getItem('bookmarks') === null) {
-        // Init array
+
+        //Init array
         var bookmarks = [];
-        // Add to array
+        //Add to array
         bookmarks.push(bookmark);
-        // set to local storage, take JSON array and change it to a string before saving in local storage.
+
+        //Set to local storage
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    } else {
-        // Get bookmarks from local storage, turn it into JSON
+    } /* So to add something else if it's already null we do this */
+    else {
+        //Get bookmarks from localStorage
         var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
         // Add bookmark to array
         bookmarks.push(bookmark);
@@ -39,32 +47,56 @@ function saveBookmark(e) {
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
 
-    // Prevent form from submitting
+    //Clear form after submission
+    document.getElementById('myForm').reset();
+
+    //Re-fetch bookmarks
+    fetchBookmarks();
+
+    //Prevent form from submitting
     e.preventDefault();
 }
 
 // Delete Bookmark
 function deleteBookmark(url) {
-    console.log(url);
+    //Must now loop through local Storage array. If passed in url matches one in localStorage we will splice it out.
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Loop through bookmarks
+    for (i = 0; i < bookmarks.length; i++) {
+        if (bookmarks[i].url == url) {
+            //Remove from array
+            bookmarks.splice(i, 1);
+        }
+    }
+    // Re-set the local storage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    //Re-fetch bookmarks
+    fetchBookmarks();
 }
 
-// Fetch bookmarks
+
+
+
+//We need to fetch bookmarks from localStorage and display them onscreen in the div
 function fetchBookmarks() {
+    // This simply defines it so on body in HTML file we need to put onload=fetchBookmarks() in <body> tag
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
 
-    // Get output ID
+    // Get output Id
     var bookmarksResults = document.getElementById('bookmarksResults');
-    // Build output
+
+    //Build output
     bookmarksResults.innerHTML = '';
-    // Loop through bookmarks in local storage and output 1 by 1 into the div
-    for (i = 0; i < bookmarks.length; i++) {
+
+    for (var i = 0; i < bookmarks.length; i++) {
         var name = bookmarks[i].name;
         var url = bookmarks[i].url;
 
-        bookmarksResults.innerHTML += '<div class="well">' +
-            '<h3>' + name + '</h3>' +
-            '<a class="btn btn-default" target="_blank" href="' + url + '">' + 'Visit' + '</a>' +
-            '<a onclick = "deleteBookmark(\' ' + url + '\')" class="btn btn-danger" href="#">' + 'Delete' + '</a>' +
+        bookmarksResults.innerHTML += '<h3>' + name +
+            ' <a class="btn btn-default" target="_blank" href="' + url + '">Visit</a> ' +
+            ' <a onclick="deleteBookmark(\'' + url + '\')" class="btn btn-danger" href="#">Delete</a> ' +
+            '</h3>' +
             '</div>';
     }
 }
